3fddda535b3b0b6cd9ae688fc006275dda828e5a
itscharukadeshan <itscharukadeshan@gmail.com>
2023-03-11 15:37:01 +0530

    Final 1.6-unicafe-step1

--- Part1/1.6 unicafe-step1/src/App.jsx
+++ Part1/1.6 unicafe-step1/src/App.jsx
@@ -8,7 +8,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
 };
 
 export default App;
