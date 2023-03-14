5be11cf2a24334d4bfeab19e2a7f0f9528c8b9b8
itscharukadeshan <itscharukadeshan@gmail.com>
2023-03-14 13:38:22 +0530

    add setNotes use state

--- Part2/2.06-2.10-the-Phonebook/src/App.jsx
+++ Part2/2.06-2.10-the-Phonebook/src/App.jsx
@@ -1,8 +1,12 @@
 /** @format */
+import React from "react";
+import { useState } from "react";
 
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+
   return (
     <div>
       <h1>Notes</h1>
