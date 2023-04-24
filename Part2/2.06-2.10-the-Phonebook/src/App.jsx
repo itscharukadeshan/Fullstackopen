315414d880ee161dbc27a5b3f29bccd7d4b514a8
itscharukadeshan <itscharukadeshan@gmail.com>
2023-04-24 14:44:36 +0530

    add notification

--- Part2/2.06-2.10-the-Phonebook/src/App.jsx
+++ Part2/2.06-2.10-the-Phonebook/src/App.jsx
@@ -1,6 +1,7 @@
 /** @format */
 import React from "react";
 import { useState } from "react";
+
 import "./index.css";
 
 const App = () => {
@@ -21,46 +22,52 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
-  const addPerson = (e) => {
-    e.preventDefault();
-    const newPerson = {
-      name: newName,
-      number: newNumber,
-      id: Math.max(...persons.map((person) => person.id)) + 1,
-    };
-    setPersons(persons.concat(newPerson));
-    setNewName("");
-    setNewNumber("");
-  };
+  const existingPerson = persons.find((person) => person.name === newName);
 
-  return (
-    <div className='p-5'>
-      <h2 className='text-5xl pb-4'>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div className='text-xl'>
-          name : <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div className='text-xl'>
-          number : <input value={newNumber} onChange={handleNumberChange} />
-        </div>
+  if (existingPerson) {
+    alert(`${newName} is already added to the phonebook`);
+  } else {
+    const addPerson = (e) => {
+      e.preventDefault();
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+        id: Math.max(...persons.map((person) => person.id)) + 1,
+      };
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+      setNewNumber("");
+    };
+
+    return (
+      <div className='p-5'>
+        <h2 className='text-5xl pb-4'>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div className='text-xl'>
+            name : <input value={newName} onChange={handleNameChange} />
+          </div>
+          <div className='text-xl'>
+            number : <input value={newNumber} onChange={handleNumberChange} />
+          </div>
+          <div>
+            <button
+              className='pl-3 pr-3 pt-1  pb-1 mt-5 rounded-md bg-black text-white hover:bg-white hover:text-black hover:border-black hover:border-solid hover:border-2'
+              type='submit'>
+              add
+            </button>
+          </div>
+        </form>
         <div>
-          <button
-            className='pl-3 pr-3 pt-1  pb-1 mt-5 rounded-md bg-black text-white hover:bg-white hover:text-black hover:border-black hover:border-solid hover:border-2'
-            type='submit'>
-            add
-          </button>
+          <h2 className='text-5xl pb-4 pt-5'>Numbers</h2>
+          {persons.map((person) => (
+            <li className=' list-none text-lg' key={person.name}>
+              {person.name} {person.number}
+            </li>
+          ))}
         </div>
-      </form>
-      <div>
-        <h2 className='text-5xl pb-4 pt-5'>Numbers</h2>
-        {persons.map((person) => (
-          <li className=' list-none text-lg' key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
