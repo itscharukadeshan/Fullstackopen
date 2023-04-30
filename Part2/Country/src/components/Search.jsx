17035cd272647f79b0a48e0b95c565eea5d27a74
itscharukadeshan <itscharukadeshan@gmail.com>
2023-04-30 19:06:42 +0530

    add some styles

--- Part2/Country/src/components/Search.jsx
+++ Part2/Country/src/components/Search.jsx
@@ -5,10 +5,10 @@ import React from "react";
 export default function Search({ handleValue, handleSearch }) {
   return (
     <div>
-      <div className='text-xl font-serif w-full bg-cyan-600 p-5 text-black'>
-        Find countries :{" "}
+      <div className='text-xl font-serif font-bold w-full bg-cyan-600 p-5 text-white'>
+        Find countries{""}
         <input
-          className=' bg-slate-400 rounded-lg border-solid border-2 border-cyan-800'
+          className=' ml-4 bg-slate-400 rounded-lg border-solid border-2 border-cyan-800'
           type='text'
           placeholder='Search for a country'
           onChange={handleValue}
