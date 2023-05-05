9b20f38692672d9ae321e84de5e54ad7e9df07b6
itscharukadeshan <itscharukadeshan@gmail.com>
2023-05-05 12:41:55 +0530

    fixed api key

--- Part2/Country/vite.config.js
+++ Part2/Country/vite.config.js
@@ -4,10 +4,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Define your environment variables here
-  define: {
-    "process.env": {
-      OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY,
-    },
-  },
   // ...other configuration options
 });
