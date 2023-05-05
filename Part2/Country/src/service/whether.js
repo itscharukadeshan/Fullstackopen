98fdbda02464377adc621fb407fc476fa382a8e6
itscharukadeshan <itscharukadeshan@gmail.com>
2023-05-05 15:11:39 +0530

    change the wether api call to only give  object

--- Part2/Country/src/service/whether.js
+++ Part2/Country/src/service/whether.js
@@ -1,22 +1,16 @@
 /** @format */
 
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const Base_Url = "https://api.openweathermap.org/data/2.5/weather?q=";
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-const whetherCapital = () => {
-  const [whether, setWhether] = useState([]);
-  const [city, setCity] = useState("");
-
+const whetherCapital = (city) => {
   useEffect(() => {
     axios
       .get(`${Base_Url}${city}&appid=${apiKey}`)
-      .then((response) => setWhether(response.data));
-  }, []);
-
-  return whether;
+      .then((response) => response.data);
+  }, [city]);
 };
 
 export default whetherCapital;
