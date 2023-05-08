d024cb339ec2c1a2c1f6d1aa85f2669220539d7d
itscharukadeshan <itscharukadeshan@gmail.com>
2023-05-08 10:26:52 +0530

    add example front/back end

---
+++ Part3/Notes/Front-end/src/components/Notification.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+const Notification = ({ message, isVisible }) => {
+  if (!isVisible || message === null) {
+    return null;
+  }
+
+  return (
+    <div className=' text-red-700 text-2xl bg-slate-400 border-solid border-red-700 border-4 rounded-md p-8  mb-10'>
+      {message}
+    </div>
+  );
+};
+
+export default Notification;
