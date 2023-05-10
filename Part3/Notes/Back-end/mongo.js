d114dc8c4d64d9d719f2b7725af3685c9af9ca16
charuka deshan <itscharukadeshan@gmail.com>
2023-05-10 11:46:33 +0530

    add note js

--- Part3/Notes/Back-end/mongo.js
+++ Part3/Notes/Back-end/mongo.js
@@ -9,27 +9,31 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://itscharukadeshan:${password}@cluster0.qjsqgi9.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: "HTML is Easy",
+  content: "Mongoose makes things easy",
+  date: new Date(),
   important: true,
 });
 
-/*note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});*/
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
 
 Note.find({}).then((result) => {
   result.forEach((note) => {
