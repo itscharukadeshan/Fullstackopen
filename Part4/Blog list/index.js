b937bc000aa1198b41a61d364b214c39e3c996f5
charuka deshan <itscharukadeshan@gmail.com>
2023-05-25 11:16:35 +0530

    add dot env

--- Part4/Blog list/index.js
+++ Part4/Blog list/index.js
@@ -3,6 +3,9 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -12,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -36,7 +39,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
