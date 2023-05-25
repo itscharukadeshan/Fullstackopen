24c936a9f82fc914d35532ff57ed0618817267b8
charuka deshan <itscharukadeshan@gmail.com>
2023-05-25 12:39:43 +0530

    4.2 Blog list, step2 complete

--- Part4/Notes/mongo.js
+++ Part4/Notes/mongo.js
@@ -21,12 +21,6 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'Mongoose makes things easy',
-  date: new Date(),
-  important: true,
-})
-
 
 Note.find({}).then(result => {
   result.forEach(note => {
