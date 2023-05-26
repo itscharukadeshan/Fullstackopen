ca4c3478f579857776a2985e4b167c989df69d2f
charuka deshan <itscharukadeshan@gmail.com>
2023-05-26 12:27:51 +0530

    4.4: helper functions/ unit tests, step2 completed

--- Part4/Blog list/utils/list_helper.js
+++ Part4/Blog list/utils/list_helper.js
@@ -1,9 +1,26 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
+
 const dummy = (blogs) => {
 
   return 1
 }
 
+
+
+const totalLikes = (blogs) => {
+  let totalLikes = 0
+
+  for (let i = 0; i < blogs.length; i++) {
+    const blog = blogs[i]
+    totalLikes += blog.likes
+  }
+
+  return totalLikes
+}
+
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
