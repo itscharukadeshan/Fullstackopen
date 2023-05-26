6658ff951da0266f67be4751973db7116707523a
charuka deshan <itscharukadeshan@gmail.com>
2023-05-26 17:52:16 +0530

    refactor the route handler to use the async/await

--- Part4/Blog list/tests/blogs_api.test.js
+++ Part4/Blog list/tests/blogs_api.test.js
@@ -4,6 +4,27 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let noteObject = new Blog(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Blog(initialNotes[1])
+  await noteObject.save()
+})
+
 
 test('blogs are returned as json', async () => {
   await api
