ca86858b7b998dae1d8848343c8f050f2521dc38
charuka deshan <itscharukadeshan@gmail.com>
2023-05-27 19:19:36 +0530

    add login

--- Part4/Notes/app.js
+++ Part4/Notes/app.js
@@ -26,9 +26,12 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+const loginRouter = require('./controllers/login')
+
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
