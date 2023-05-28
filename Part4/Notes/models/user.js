a6397de04344532c301c6ccaedb2a660f42f7af6
charuka deshan <itscharukadeshan@gmail.com>
2023-05-28 10:17:04 +0530

    updated missing req

--- Part4/Notes/models/user.js
+++ Part4/Notes/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -17,17 +17,17 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
 })
 
-userSchema.plugin(uniqueValidator)
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
