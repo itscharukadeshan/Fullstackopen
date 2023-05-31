d0fda9f3cd73977d4d42af688b987b9044de1182
charuka deshan <itscharukadeshan@gmail.com>
2023-05-31 14:55:36 +0530

    4.20*: bloglist expansion, step8 completed

--- Part4/Blog list/controllers/blogs.js
+++ Part4/Blog list/controllers/blogs.js
@@ -3,13 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require ('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user',{ username: 1 , name: 1 , id :1 })
@@ -32,9 +25,12 @@ blogsRouter.get('/:id', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
+
   const body = request.body
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const token = request.token
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
