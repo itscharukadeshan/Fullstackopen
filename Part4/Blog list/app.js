fd20e74fadd815d692571c305f0127b4f9576ca9
charuka deshan <itscharukadeshan@gmail.com>
2023-05-31 14:52:56 +0530

    add middleware to app js to extract token and user

--- Part4/Blog list/app.js
+++ Part4/Blog list/app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
+const { requestLogger,unknownEndpoint,errorHandler,tokenExtractor,userExtractor } = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -26,13 +26,13 @@ mongoose
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(requestLogger)
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter,userExtractor,tokenExtractor)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 module.exports = app
