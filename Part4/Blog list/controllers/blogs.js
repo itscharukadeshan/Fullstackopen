dbc4994448a8c1ed9df4c61f734175d441e4a765
charuka deshan <itscharukadeshan@gmail.com>
2023-05-31 11:08:34 +0530

    4.19: bloglist expansion, step7 completed

--- Part4/Blog list/controllers/blogs.js
+++ Part4/Blog list/controllers/blogs.js
@@ -1,5 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require ('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -23,16 +33,27 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+
   const blog = await new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id
+
   })
 
   try {
-    const saveBlog = await blog.save()
-    response.status(201).json(saveBlog)
+    const savedBlog = await blog.save()
+    await user.save()
+    response.status(201).json(savedBlog)
+
   } catch (exception) {
     next(exception)
   }
