1bc12ab0eba3c06533b1645a714ea784fe1fb7a9
charuka deshan <itscharukadeshan@gmail.com>
2023-06-05 09:30:04 +0530

    add handle login middleware

--- Part4/Blog list/tests/test_helpers.js
+++ Part4/Blog list/tests/test_helpers.js
@@ -1,5 +1,7 @@
 const Blog = require('../models/blog')
 const User = require ('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 
 const nonExistingId = async () => {
@@ -30,8 +32,32 @@ const oneUserInDb = async () => {
   const user = await User.find([0])
   return user.map (user => user.toJSON())
 }
+const logInUser = async (username, password) => {
+  try {
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      throw new Error('Invalid username or password')
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid username or password')
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.SECRET, { expiresIn: '1h' })
+
+    return token
+
+  } catch (error) {
+    console.error(error)
+    throw new Error('Login failed')
+  }
+}
+
 
 
 module.exports = {
-  nonExistingId, blogsInDb,oneUserInDb,usersInDb
+  nonExistingId, blogsInDb,oneUserInDb,usersInDb,logInUser
 }
