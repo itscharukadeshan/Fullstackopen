e98dc5eef09946ea41698e14a0739b0aadfd63ea
charuka deshan <itscharukadeshan@gmail.com>
2023-06-09 15:09:48 +0530

    add initial code

--- Part5/Bloglist-Frontend/src/App.jsx
+++ Part5/Bloglist-Frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
 
-function App() {
-  return <div>App</div>
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
 }
 
 export default App
