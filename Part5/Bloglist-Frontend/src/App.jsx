d3e46b6595bea81f5abbb6c28288477b43b41e9b
charuka deshan <itscharukadeshan@gmail.com>
2023-06-11 11:47:19 +0530

    5.2: bloglist frontend, step 1- 2 complete

--- Part5/Bloglist-Frontend/src/App.jsx
+++ Part5/Bloglist-Frontend/src/App.jsx
@@ -6,13 +6,27 @@ import LoginForm from './components/LoginForm'
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [token, setToken] = useState('')
 
+  useEffect(() => {
+    const storedUserData = localStorage.getItem('userData')
+    if (storedUserData) {
+      const userData = JSON.parse(storedUserData)
+      setUser(userData)
+      setToken(`Bearer ${userData.token}`)
+    }
+  }, [])
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
-  const handleLogin = (userData) => {
-    setUser(userData)
+  const handleLogin = async (userData) => {
+    await setUser(userData)
+    localStorage.setItem('userData', JSON.stringify(userData))
+  }
+  const handleLogout = () => {
+    setUser(null)
+    localStorage.removeItem('userData')
   }
 
   if (user === null) {
@@ -20,11 +34,24 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h2 className="py-6 font-mono font-bold text-2xl">blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+    <div className="pl-2 pt-6">
+      <div className="pb-6 text-2xl flex flex-row items-center gap-4 border-b-2 border-gray-500">
+        <div>
+          {user.name} is logged in{'  '}
+        </div>
+        <button
+          className="btn btn-xs btn-outline btn-warning  "
+          onClick={handleLogout}
+        >
+          Logout
+        </button>{' '}
+      </div>
+      <h2 className="py-6 font-mono font-bold text-4xl">blogs</h2>
+      <div className=" font-bold py-2">
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
     </div>
   )
 }
