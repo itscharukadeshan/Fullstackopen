efb5668ce9446709ae322a6f5116b62f6399cbf7
charuka deshan <itscharukadeshan@gmail.com>
2023-06-11 10:20:55 +0530

    create basic form structure

--- Part5/Bloglist-Frontend/src/components/LoginForm.jsx
+++ Part5/Bloglist-Frontend/src/components/LoginForm.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
+import { useState } from 'react'
 
 function LoginForm() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const credentials = {
+      username,
+      password,
+    }
+    setPassword('')
+    setUsername('')
+
+    console.log(credentials)
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
   return (
     <div className="pl-2">
       <h2 className="py-6 font-mono font-bold text-2xl">Log in to blog list</h2>
       <form
         className="flex flex-col gap-4 justify-center"
-        action="http://localhost:3000/api/users"
-        method="post"
+        onSubmit={handleSubmit}
       >
         <input
           type="text"
           placeholder="Type username here"
           className="input input-bordered input-info w-full max-w-xs"
+          value={username}
+          onChange={handleUsernameChange}
         />
         <input
           type="password"
           placeholder="Type password here"
           className="input input-bordered input-info w-full max-w-xs"
+          value={password}
+          onChange={handlePasswordChange}
         />
         <button type="submit" className="btn btn-outline btn-accent w-fit my-6">
           Log in
