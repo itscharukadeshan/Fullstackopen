7d6bdf6ab306c6deebb935613a313fdda0284ed0
charuka deshan <itscharukadeshan@gmail.com>
2023-06-25 12:41:55 +0530

    fix styling issue

--- Part5/Notes/src/App.jsx
+++ Part5/Notes/src/App.jsx
@@ -113,7 +113,7 @@ const App = () => {
 
         <div>
           <a
-            className=" mt-16 w-fit flex items-center justify-center gap-2 rounded-xl border-4 border-black bg-blue-100 px-3 py-2 text-sm shadow-[6px_6px_0_0_#000] transition hover:cursor-pointer hover:shadow-none focus:outline-none focus:ring active:bg-blue-50"
+            className=" mt-8 mb-16 w-fit flex items-center justify-center gap-2 rounded-xl border-4 border-black bg-blue-100 px-3 py-2 text-sm shadow-[6px_6px_0_0_#000] transition hover:cursor-pointer hover:shadow-none focus:outline-none focus:ring active:bg-blue-50"
             onClick={() => setShowAll(!showAll)}
           >
             show {showAll ? 'important' : 'all'}
