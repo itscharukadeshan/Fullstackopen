ae7ef25317e4c7697cbf39863321e83e03006c04
charuka deshan <itscharukadeshan@gmail.com>
2023-06-29 14:23:26 +0530

    add cypress config

---
+++ Part5/Notes/cypress/e2e/note_app.cy.js
@@ -0,0 +1,14 @@
+describe('Note app', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5173/')
+    cy.contains('Notes')
+    cy.contains(
+      'Note app, Department of Computer Science, University of Helsinki 2023'
+    )
+  })
+
+  it('front page contains random text', function () {
+    cy.visit('http://localhost:5173/')
+    cy.contains('wtf is this app?')
+  })
+})
