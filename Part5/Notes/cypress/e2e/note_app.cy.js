63451980dfbcb1ac3519c3bfc1f59cb49b92bc39
charuka deshan <itscharukadeshan@gmail.com>
2023-06-30 10:14:09 +0530

    update the test

--- Part5/Notes/cypress/e2e/note_app.cy.js
+++ Part5/Notes/cypress/e2e/note_app.cy.js
@@ -1,6 +1,6 @@
 describe('Note app', function () {
   beforeEach(function () {
-    cy.visit('http://localhost:5173')
+    cy.visit(`${Cypress.env('baseUrl')}`)
   })
 
   it('front page can be opened', function () {
@@ -22,5 +22,21 @@ describe('Note app', function () {
     cy.get('#login-button').click()
 
     cy.contains('charuka logged in')
+  })
+
+  describe('When log in ', function () {
+    it('login fails with wrong password', function () {
+      cy.contains('log in').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+    })
   })
 })
