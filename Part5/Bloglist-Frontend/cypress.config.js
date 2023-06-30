b6556cd8ef8cf948a70c118111704110baa0e1b6
charuka deshan <itscharukadeshan@gmail.com>
2023-06-30 11:42:27 +0530

    fix the api end points

--- Part5/Bloglist-Frontend/cypress.config.js
+++ Part5/Bloglist-Frontend/cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     e2e: {
-      baseUrl: 'http://localhost:3000',
+      baseUrl: 'http://localhost:5173',
     },
     env: {
       BACKEND: 'http://localhost:3001/api',
