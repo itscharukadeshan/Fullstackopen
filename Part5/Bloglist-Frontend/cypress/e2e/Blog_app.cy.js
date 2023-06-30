717fb52be23e1c8b756b43b82ff84e9737f2d150
charuka deshan <itscharukadeshan@gmail.com>
2023-06-30 11:55:06 +0530

    update the structure

--- Part5/Bloglist-Frontend/cypress/e2e/Blog_app.cy.js
+++ Part5/Bloglist-Frontend/cypress/e2e/Blog_app.cy.js
@@ -1,5 +1,29 @@
-describe('Blog', () => {
-  it('can be visit the', () => {
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', `${Cypress.env('BACKEND')}/api/testing/reset`)
     cy.visit(`${Cypress.env('baseUrl')}`)
+  })
+
+  it('Login form is shown', function () {
+    // ...
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      // ...
+    })
+
+    it('fails with wrong credentials', function () {
+      // ...
+    })
+  })
+  describe('When logged in', function () {
+    beforeEach(function () {
+      // log in user here
+    })
+
+    it('A blog can be created', function () {
+      // ...
+    })
   })
 })
