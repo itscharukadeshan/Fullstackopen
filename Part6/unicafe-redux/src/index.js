9f22829379727f2f26e540ede5263ae3a273da52
charuka deshan <itscharukadeshan@gmail.com>
2023-07-11 12:02:46 +0530

    add styling / part 2 complete

--- Part6/unicafe-redux/src/index.js
+++ Part6/unicafe-redux/src/index.js
@@ -6,6 +6,8 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import reducer from "./reducer";
 
+import "./index.css";
+
 const store = createStore(reducer);
 
 const App = () => {
@@ -34,14 +36,44 @@ const App = () => {
   };
 
   return (
-    <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>ok</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={resetStats}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
+    <div className='flex flex-col gap-4 items-center p-8'>
+      <div className='flex flex-row gap-2 mt-4'>
+        <button className='btn btn-sm btn-outline' onClick={good}>
+          good
+        </button>
+        <button className='btn btn-sm btn-outline' onClick={ok}>
+          ok
+        </button>
+        <button className='btn btn-sm btn-outline' onClick={bad}>
+          bad
+        </button>
+        <button className='btn btn-sm btn-outline' onClick={resetStats}>
+          reset stats
+        </button>
+      </div>
+
+      <div>
+        <div className='stats stats-vertical lg:stats-horizontal shadow'>
+          <div className='stat'>
+            <div className='stat-title'>Good</div>
+            <div className='stat-value text-green-600'>
+              {store.getState().good}
+            </div>
+          </div>
+
+          <div className='stat'>
+            <div className='stat-title'>Ok</div>
+            <div className='stat-value text-warning'>{store.getState().ok}</div>
+          </div>
+
+          <div className='stat'>
+            <div className='stat-title'>Bad</div>
+            <div className='stat-value text-red-600'>
+              {store.getState().bad}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
