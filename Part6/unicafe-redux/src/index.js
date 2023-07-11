eecbea9ef61a411b79e83b9a46878f15778bb17c
charuka deshan <itscharukadeshan@gmail.com>
2023-07-11 11:36:26 +0530

    6.1: unicafe revisited, step1 complete

---
+++ Part6/unicafe-redux/src/index.js
@@ -0,0 +1,56 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: "GOOD",
+    });
+  };
+
+  const ok = () => {
+    store.dispatch({
+      type: "OK",
+    });
+  };
+
+  const bad = () => {
+    store.dispatch({
+      type: "BAD",
+    });
+  };
+
+  const resetStats = () => {
+    store.dispatch({
+      type: "ZERO",
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={resetStats}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+};
+
+const root = document.getElementById("root");
+
+const renderApp = () => {
+  ReactDOM.render(<App />, root);
+};
+
+renderApp();
+store.subscribe(renderApp);
