74de4f2ab48864f3ac5c02b9f1f1a790bc1a1046
charuka deshan <itscharukadeshan@gmail.com>
2023-07-11 13:43:06 +0530

    6.5: anecdotes, step3  complete

--- Part6/redux-anecdotes/src/App.js
+++ Part6/redux-anecdotes/src/App.js
@@ -4,7 +4,10 @@ import { useSelector, useDispatch } from "react-redux";
 import "./index.css";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) =>
+    state.sort((a, b) => b.votes - a.votes)
+  );
+
   const dispatch = useDispatch();
 
   const vote = (id) => {
