542c3673537e50333cbc6f966c56a166d6fd11d7
charuka deshan <itscharukadeshan@gmail.com>
2023-07-13 20:37:32 +0530

    add db using json server

--- Part6/redux-notes/src/App.js
+++ Part6/redux-notes/src/App.js
@@ -1,11 +1,20 @@
 /** @format */
-
-import Notes from "./components/Notes";
+import { useEffect } from "react";
 import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
+import noteService from "./services/notes";
+import { setNotes } from "./reducers/noteReducer";
+import { useDispatch } from "react-redux";
+
 import "./index.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    noteService.getAll().then((notes) => dispatch(setNotes(notes)));
+  }, [dispatch]);
+
   return (
     <div className='flex flex-col align-baseline items-center my-8 mx-4'>
       <NewNote />
--- Part6/redux-notes/src/index.js
+++ Part6/redux-notes/src/index.js
@@ -8,8 +8,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, { setNotes } from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
+
+import noteService from "./services/notes";
 
 const store = configureStore({
   reducer: {
@@ -18,7 +20,7 @@ const store = configureStore({
   },
 });
 
-console.log(store.getState());
+noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
