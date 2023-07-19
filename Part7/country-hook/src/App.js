49e65d0146401dc1272b54c73bfb9574f14a9858
charuka deshan <itscharukadeshan@gmail.com>
2023-07-19 12:58:50 +0530

    add basic structure and useEffect using useCountry

---
+++ Part7/country-hook/src/App.js
@@ -0,0 +1,106 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    axios
+      .get(`https://restcountries.com/v2/name/${name}`)
+      .then((response) => {
+        setCountry(response.data[0]);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [name]);
+
+  return {
+    country,
+    loading,
+    error,
+  };
+};
+
+const Country = ({ country }) => {
+  const { isLoading, error, country: countryData } = country;
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error === null) {
+    return <></>;
+  }
+
+  if (error) {
+    return <div>Error fetching country</div>;
+  }
+
+  if (!countryData) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h3>{countryData.name}</h3>
+
+      <img
+        src={countryData.flags.png}
+        height='100'
+        alt={`the flag of ${countryData.name}`}
+      />
+
+      <div>Capital : {countryData.capital}</div>
+
+      <div>population : {countryData.population}</div>
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
