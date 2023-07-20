fef350003a49346ecdd616baadab5137fbb16d0c
charuka deshan <itscharukadeshan@gmail.com>
2023-07-20 20:05:30 +0530

    add notification slices

---
+++ Part7/Bloglist-query/src/store/Slices/notificationSlice.js
@@ -0,0 +1,24 @@
+// notificationSlice.js
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  errors: '',
+}
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    addNotification(state, action) {
+      state.notifications.push(action.payload)
+    },
+    removeNotification(state, action) {
+      state.notifications = state.notifications.filter(
+        (notification) => notification.id !== action.payload
+      )
+    },
+  },
+})
+
+export const { addNotification, removeNotification } = notificationSlice.actions
+export default notificationSlice.reducer
--- Part7/Bloglist-query/src/store/index.js
+++ Part7/Bloglist-query/src/store/index.js
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
-const reducer = combineReducers({})
+
+import notificationSliceReducer from './Slices/notificationSlice'
+
+const reducer = combineReducers({
+  notifications: notificationSliceReducer,
+})
+
 const store = configureStore({
   reducer,
 })
+
 export default store
