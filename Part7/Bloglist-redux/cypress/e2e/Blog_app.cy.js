09b26e86b1da212576a69b82a02b34420d30985f
charuka deshan <itscharukadeshan@gmail.com>
2023-07-20 10:46:11 +0530

    add starter code

---
+++ Part7/Bloglist-redux/cypress/e2e/Blog_app.cy.js
@@ -0,0 +1,55 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.visit('')
+  })
+
+  it('Login form is shown', function () {
+    cy.contains('Log in to blog list')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('lord')
+      cy.get('#password').type('lord')
+      cy.get('#login-button').click()
+      cy.contains('lord is logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('lord')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+      cy.get('.Toastify__toast-body > :nth-child(2)').contains('Login failed')
+    })
+  })
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.get('#username').type('lord')
+      cy.get('#password').type('lord')
+      cy.get('#login-button').click()
+    })
+
+    it('A blog can be created', function () {
+      cy.get('#new-blog').click()
+      cy.get('#title').type('a note created by cypress')
+      cy.get('#author').type('Cypress inc')
+      cy.get('#url').type('https://example.com/cypress')
+      cy.get('#submit').click()
+      cy.reload()
+      cy.contains('a note created by cypress')
+    })
+    it('create blog can be cancel', function () {
+      cy.get('#new-blog').click()
+      cy.get('#cancel').click()
+    })
+
+    it('users can like a blog', function () {})
+    it('user who created a blog can delete it', function () {})
+    it('only the creator can see the delete button of a blog', function () {})
+    it('users can like a blog', function () {})
+  })
+
+  afterEach(function () {
+    cy.wait(1000)
+  })
+})
