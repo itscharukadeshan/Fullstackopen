e3b4a47b73045d64455c67624e07d1a39a4a3574
charuka deshan <itscharukadeshan@gmail.com>
2023-07-21 21:38:13 +0530

    update notification

--- Part7/Bloglist-redux/src/components/Notification.jsx
+++ Part7/Bloglist-redux/src/components/Notification.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { hideNotification } from '../store/Slices/notificationSlice'
 import { ToastContainer, toast } from 'react-toastify'
@@ -7,11 +8,13 @@ const Notification = () => {
   const notification = useSelector((state) => state.notifications)
   const dispatch = useDispatch()
 
-  if (notification.message && notification.type) {
-    toast[notification.type](notification.message, {
-      onClose: () => dispatch(hideNotification()),
-    })
-  }
+  useEffect(() => {
+    if (notification.message && notification.type) {
+      toast[notification.type](notification.message, {
+        onClose: () => dispatch(hideNotification()),
+      })
+    }
+  }, [notification, dispatch])
 
   return <ToastContainer />
 }
