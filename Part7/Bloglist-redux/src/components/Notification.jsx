486a9a17d6ff4a29364c934b83d743184628b709
charuka deshan <itscharukadeshan@gmail.com>
2023-07-21 21:28:37 +0530

    remove useEffect

--- Part7/Bloglist-redux/src/components/Notification.jsx
+++ Part7/Bloglist-redux/src/components/Notification.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { hideNotification } from '../store/Slices/notificationSlice'
 import { ToastContainer, toast } from 'react-toastify'
@@ -8,13 +7,11 @@ const Notification = () => {
   const notification = useSelector((state) => state.notifications)
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (notification.message && notification.type) {
-      toast[notification.type](notification.message, {
-        onClose: () => dispatch(hideNotification()),
-      })
-    }
-  }, [notification, dispatch])
+  if (notification.message && notification.type) {
+    toast[notification.type](notification.message, {
+      onClose: () => dispatch(hideNotification()),
+    })
+  }
 
   return <ToastContainer />
 }
