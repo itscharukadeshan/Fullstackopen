6c9f8a7925a7a99133a27c1b6dc8aa3ea2a44aff
charuka deshan <itscharukadeshan@gmail.com>
2023-07-25 10:49:01 +0530

    add persist-redux to save  data after refresh

--- Part7/Bloglist-redux/src/store/index.js
+++ Part7/Bloglist-redux/src/store/index.js
@@ -1,18 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import { persistStore } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
 
 import notificationReducer from './Slices/notificationSlice'
 import blogReducer from './Slices/blogsSlice'
 import loginReducer from './Slices/loginSlice'
 
-const reducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['login'],
+}
+
+const rootReducer = combineReducers({
   blogs: blogReducer,
   login: loginReducer,
   notifications: notificationReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
 })
 
-export default store
+const persistor = persistStore(store)
+
+export { store, persistor }
