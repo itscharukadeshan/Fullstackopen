6b1fa52198e6a4a1a4de0adcfda84e123185a64a
charuka deshan <itscharukadeshan@gmail.com>
2023-07-26 14:37:51 +0530

    add styling

--- Part7/Bloglist-redux/src/components/Blog.jsx
+++ Part7/Bloglist-redux/src/components/Blog.jsx
@@ -13,11 +13,20 @@ export default function Blog() {
   }
 
   return (
-    <div>
-      <h1> {blog.title}</h1>
-      <div>{blog.author}</div>
-      <div>{blog.url}</div>
-      <div>{blog.user[0].name}</div>
+    <div className="card w-96 bg-gray-800 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title text-4xl">{blog.title}</h2>
+        <p className="font-mono">Author {blog.author}</p>
+        <p className="font-mono">Url {blog.url}</p>
+        <div className="card-actions justify-end">
+          by
+          <Link to={`/users/${blog.user[0].id}`}>
+            <div className="underline hover:text-accent">
+              {blog.user[0].username}
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
