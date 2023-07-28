f3cf3d5361e1cfbc04f8c938f3114a8aff767301
charuka deshan <itscharukadeshan@gmail.com>
2023-07-28 09:56:01 +0530

    add use reducer and contex with custom hook

---
+++ Part7/Bloglist-query/src/hooks/useNotification.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react'
+import { NotificationContext } from './NotificationContext'
+
+export const useNotification = () => {
+  const context = useContext(NotificationContext)
+
+  if (!context) {
+    throw new Error(
+      'useNotification must be used within a NotificationProvider'
+    )
+  }
+
+  const { state, dispatch } = context
+
+  const addNotification = (message, type) => {
+    const notification = {
+      id: Date.now(),
+      message,
+      type,
+    }
+    dispatch({ type: 'ADD_NOTIFICATION', payload: notification })
+  }
+
+  const removeNotification = (id) => {
+    dispatch({ type: 'REMOVE_NOTIFICATION', payload: id })
+  }
+
+  return {
+    notifications: state.notifications,
+    addNotification,
+    removeNotification,
+  }
+}
---
+++ Part7/Bloglist-query/src/state/NotificationsContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useReducer, useEffect } from 'react'
+import { toast } from 'react-toastify'
+
+const initialState = {
+  notifications: [],
+}
+
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_NOTIFICATION':
+      return {
+        ...state,
+        notifications: [...state.notifications, action.payload],
+      }
+    case 'REMOVE_NOTIFICATION':
+      return {
+        ...state,
+        notifications: state.notifications.filter(
+          (notification) => notification.id !== action.payload
+        ),
+      }
+    default:
+      return state
+  }
+}
+
+export const NotificationContext = createContext()
+
+export const NotificationProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(notificationReducer, initialState)
+
+  useEffect(() => {
+    if (state.notifications.length > 0) {
+      const latestNotification =
+        state.notifications[state.notifications.length - 1]
+      toast[latestNotification.type](latestNotification.message, {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+      })
+
+      const timeoutId = setTimeout(() => {
+        dispatch({
+          type: 'REMOVE_NOTIFICATION',
+          payload: latestNotification.id,
+        })
+      }, 3000)
+
+      return () => {
+        clearTimeout(timeoutId)
+      }
+    }
+  }, [state.notifications])
+
+  return (
+    <NotificationContext.Provider value={{ state, dispatch }}>
+      {children}
+    </NotificationContext.Provider>
+  )
+}
