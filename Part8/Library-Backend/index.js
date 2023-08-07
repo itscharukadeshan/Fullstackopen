a47ed51d3ff39170024955163978dbd36b4c09cf
charuka deshan <itscharukadeshan@gmail.com>
2023-08-07 11:17:21 +0530

    8.3: All authors completed

--- Part8/Library-Backend/index.js
+++ Part8/Library-Backend/index.js
@@ -90,11 +90,19 @@ type Book {
   genres: [String!]!
   id: ID! 
 }
+type Author {
+  name:String!
+  born:Int!
+  bookCount:Int!
+  id:ID!
+
+}
 
   type Query {
     bookCount: Int
     authorCount: Int
     allBooks:[Book!]!
+    allAuthors:[Author!]!
 
   }
 `;
@@ -104,6 +112,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter((book) => book.author === root.name).length;
+    },
   },
 };
 
