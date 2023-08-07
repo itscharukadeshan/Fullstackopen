cace7c1e3db38d50d873a58f7299dda52efb3c9e
charuka deshan <itscharukadeshan@gmail.com>
2023-08-08 00:52:40 +0530

    fix the home add styles and icons

--- Part8/Library-Frontend/src/pages/Home.jsx
+++ Part8/Library-Frontend/src/pages/Home.jsx
@@ -1,19 +1,30 @@
 /** @format */
 import { Link } from "react-router-dom";
+import { GiBookshelf } from "react-icons/gi";
+import { ImProfile } from "react-icons/im";
+import { BiBookAdd } from "react-icons/bi";
 
 function Home() {
   return (
-    <div className='flex flex-col items-center gap-4 my-24'>
+    <div className='flex flex-col items-center gap-4 my-44'>
       <Link to={"books"}>
-        {" "}
-        <button className='btn btn-warning btn-outline'>Books</button>
+        <button className='btn btn-warning btn-outline'>
+          <GiBookshelf />
+          Books
+        </button>
       </Link>
       <Link to={"authors"}>
-        <button className='btn btn-warning btn-outline'>Authors</button>
+        <button className='btn btn-warning btn-outline'>
+          <ImProfile />
+          Authors
+        </button>
       </Link>
       <Link to={"add-book"}>
         {" "}
-        <button className='btn btn-warning btn-outline'>Add Books</button>
+        <button className='btn btn-warning btn-outline'>
+          <BiBookAdd />
+          Add Books
+        </button>
       </Link>
     </div>
   );
