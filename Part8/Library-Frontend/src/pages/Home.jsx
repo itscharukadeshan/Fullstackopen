07f0b6749851b869fa2aa812fb0042c798261f12
charuka deshan <itscharukadeshan@gmail.com>
2023-08-08 00:46:02 +0530

    add btn to home

--- Part8/Library-Frontend/src/pages/Home.jsx
+++ Part8/Library-Frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 /** @format */
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='flex flex-col items-center gap-4 my-24'>
+      <Link to={"books"}>
+        {" "}
+        <button className='btn btn-warning btn-outline'>Books</button>
+      </Link>
+      <Link to={"authors"}>
+        <button className='btn btn-warning btn-outline'>Authors</button>
+      </Link>
+      <Link to={"add-book"}>
+        {" "}
+        <button className='btn btn-warning btn-outline'>Add Books</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
