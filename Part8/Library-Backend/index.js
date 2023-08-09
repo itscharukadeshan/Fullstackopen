1ed42a013cd1c00463b8704133d25b707ccb58d6
Charuka Deshan <itscharukadeshan@gmail.com>
2023-08-09 09:06:27 +0530

    remove un use data

--- Part8/Library-Backend/index.js
+++ Part8/Library-Backend/index.js
@@ -2,7 +2,6 @@
 
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-const { v1: uuid } = require("uuid");
 
 const mongoose = require("mongoose");
 const Author = require("./models/author");
@@ -16,84 +15,6 @@ async function connect() {
 }
 
 connect();
-
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "The Demon ",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
 
 const typeDefs = `
 
@@ -154,25 +75,43 @@ const resolvers = {
 
   Mutation: {
     addBook: async (parent, args) => {
-      let author = await Author.findOne({ name: args.author });
-      if (!author) {
-        author = new Author({ name: args.author });
-        await author.save();
-      }
+      try {
+        let author = await Author.findOne({ name: args.author });
 
-      const newBook = new Book({ ...args, author: author._id });
-      await newBook.save();
-      return newBook;
+        if (!author) {
+          author = new Author({ name: args.author });
+          await author.save();
+        }
+
+        const newBook = new Book({ ...args, author: author._id });
+        await newBook.save();
+        return newBook;
+      } catch (error) {
+        if (error.code === 11000) {
+          throw new GraphQLError("Book already exists");
+        } else {
+          throw new GraphQLError("Error adding book");
+        }
+      }
     },
 
     editAuthor: async (parent, args) => {
-      const { name, setBornTo } = args;
-      const author = await Author.findOne({ name });
-      if (!author) throw "Author not found";
+      try {
+        const { name, setBornTo } = args;
 
-      author.born = setBornTo;
-      await author.save();
-      return author;
+        const author = await Author.findOne({ name });
+
+        author.born = setBornTo;
+        await author.save();
+
+        return author;
+      } catch (error) {
+        if (error.kind === "ObjectId") {
+          throw new GraphQLError("Author not found");
+        } else {
+          throw new GraphQLError("Error updating author");
+        }
+      }
     },
   },
 };
