d70f41b2d7f9e515a8dcb9c32212773916eeebe0
Charuka Deshan <itscharukadeshan@gmail.com>
2023-08-10 08:19:42 +0530

    add login form

--- Part8/Library-Frontend/src/App.jsx
+++ Part8/Library-Frontend/src/App.jsx
@@ -1,14 +1,18 @@
 /** @format */
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 import Home from "./pages/Home";
 import Authors from "./pages/Authors";
 import AddBook from "./pages/AddBook";
 import Books from "./pages/Books";
+import Login from "./pages/Login";
 
 import Navigation from "./components/Navigation";
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
   return (
     <div>
       <Navigation />
