3453021a6fa5f004c94de02e185dcbdeb9c306d5
Charuka Deshan <itscharukadeshan@gmail.com>
2023-08-12 11:45:05 +0530

    add basic structure for recommendations

--- Part8/Library-Frontend/src/pages/Recommendations.jsx
+++ Part8/Library-Frontend/src/pages/Recommendations.jsx
@@ -1,9 +1,22 @@
 /** @format */
 
-import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../queries/userQueries";
 
 function Recommendations() {
-  return <div>Recommendations</div>;
+  const { loading, data, error } = useQuery(GET_USER);
+
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center h-full'>
+        <span className='loading'></span>
+      </div>
+    );
+  }
+
+  if (error) return <p>Error fetching books!</p>;
+
+  console.log(data);
 }
 
 export default Recommendations;
