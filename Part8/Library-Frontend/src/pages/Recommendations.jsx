053afc81807d5fb497821a026da514e5d6b7cbc4
Charuka Deshan <itscharukadeshan@gmail.com>
2023-08-14 09:44:40 +0530

    add data display in component

--- Part8/Library-Frontend/src/pages/Recommendations.jsx
+++ Part8/Library-Frontend/src/pages/Recommendations.jsx
@@ -16,6 +16,13 @@ function Recommendations() {
 
   if (result.error) return <p>Error fetching books!</p>;
 
+  return (
+    <main className='flex flex-col gap-3'>
+      <h2 className='text-xl font-bold'>Recommendation</h2>
+      <div>book in your favorite genre {result.data.me.favoriteGenre}</div>
+    </main>
+  );
+
   console.log(result.data);
 }
 
