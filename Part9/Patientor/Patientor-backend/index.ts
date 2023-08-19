fa2dcff0c83c6bd1a237fc1b3339b964f643410e
charuka deshan <itscharukadeshan@gmail.com>
2023-08-19 09:53:17 +0530

    9.9: Patientor backend, step2 completed

--- Part9/Patientor/Patientor-backend/index.ts
+++ Part9/Patientor/Patientor-backend/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   const response = {
