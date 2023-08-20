613ad14c78af5295b7ec6de8b8ea9a7e822deefd
charuka deshan <itscharukadeshan@gmail.com>
2023-08-20 12:22:16 +0530

    add get req to the app

--- Part9/Flight-diary/flight-diary-frontend/src/App.tsx
+++ Part9/Flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,7 +1,29 @@
 /** @format */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Entry } from "./types";
+
 import DiaryEntry from "./components/DiaryEntry";
 
 function App() {
+  const [entries, setEntries] = useState<Entry[]>([]);
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      try {
+        const response = await axios.get<Entry[]>(
+          "http://localhost:3000/api/diaries"
+        );
+        setEntries(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchEntries();
+  }, []);
+
   return (
     <>
       <DiaryEntry />
---
+++ Part9/Flight-diary/flight-diary-frontend/src/types.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+// types.ts
+
+export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "snowy";
+
+export type Visibility = "great" | "good" | "ok" | "poor";
+
+export interface Entry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment?: string;
+}
