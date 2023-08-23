e1b4b4ac5a2ebff60fc188a586626a11085f4877
charuka deshan <itscharukadeshan@gmail.com>
2023-08-23 16:10:33 +0530

    add Entry types

--- Part9/Patientor/Patientor-frontend/src/types.ts
+++ Part9/Patientor/Patientor-frontend/src/types.ts
@@ -17,7 +17,6 @@ export interface BaseEntry {
   description: string;
   date: string;
   specialist: string;
-  type: string;
 
   diagnosisCodes?: Array<Diagnosis["code"]>;
 }
@@ -31,5 +30,45 @@ export interface Patient {
   dateOfBirth?: string;
   entries?: BaseEntry[];
 }
+
+export enum EntryType {
+  HealthCheck = "HealthCheck",
+  OccupationalHealthCare = "OccupationalHealthcare",
+  Hospital = "Hospital",
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: EntryType.HealthCheck;
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: EntryType.OccupationalHealthCare;
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: EntryType.Hospital;
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
