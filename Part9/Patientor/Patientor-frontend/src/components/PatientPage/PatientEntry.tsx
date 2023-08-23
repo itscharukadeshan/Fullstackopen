0331c47c16aa5a02ca5c2426dfe1fccfe58c7bd2
charuka deshan <itscharukadeshan@gmail.com>
2023-08-23 21:35:41 +0530

    9.25: Patientor, step6 completed

--- Part9/Patientor/Patientor-frontend/src/components/PatientPage/PatientEntry.tsx
+++ Part9/Patientor/Patientor-frontend/src/components/PatientPage/PatientEntry.tsx
@@ -1,6 +1,7 @@
 /** @format */
 import { useState, useEffect } from "react";
 import { BaseEntry, Diagnosis } from "../../types";
+import { Card } from "@mui/material";
 import diagnoseServices from "../../services/diagnoses";
 import HospitalEntry from "./HospitalEntry";
 import OccupationalEntry from "./OccupationalEntry";
@@ -52,7 +53,16 @@ function PatientEntry({ entry }: Props) {
       entryComponent = <p>Unknown entry type</p>;
   }
 
-  return <>{entryComponent}</>;
+  return (
+    <Card
+      style={{
+        padding: "50px",
+        border: "2px solid rgba(0, 0, 0, 0.3)",
+        margin: "10px",
+      }}>
+      {entryComponent}
+    </Card>
+  );
 }
 
 export default PatientEntry;
